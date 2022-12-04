39e7a4fc4d427237ef40e43bfe85a1f42484487d
Joost Farla <joost.farla@kadaster.nl>
2022-12-04 19:08:28 +0100

    Additional scalar types (2)

--- model/src/main/java/org/dotwebstack/orchestrate/model/type/ScalarTypes.java
+++ model/src/main/java/org/dotwebstack/orchestrate/model/type/ScalarTypes.java
@@ -6,7 +6,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ScalarTypes {
 
+  public static final BooleanType BOOLEAN = new BooleanType();
+
+  public static final DoubleType DOUBLE = new DoubleType();
+
+  public static final FloatType FLOAT = new FloatType();
+
   public static final IntegerType INTEGER = new IntegerType();
+
+  public static final LongType LONG = new LongType();
 
   public static final StringType STRING = new StringType();
 }
