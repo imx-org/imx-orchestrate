8da482e8b7180e8a56719624007ff9ac4a57e6b6
Joost Farla <joost.farla@kadaster.nl>
2023-02-07 14:55:26 +0100

    Make gateway stub smarter

--- gateway/src/main/java/org/dotwebstack/orchestrate/GatewayConfiguration.java
+++ gateway/src/main/java/org/dotwebstack/orchestrate/GatewayConfiguration.java
@@ -4,7 +4,9 @@ import static org.dotwebstack.orchestrate.TestFixtures.createModelMapping;
 
 import graphql.GraphQL;
 import graphql.schema.GraphQLSchema;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Optional;
 import org.dotwebstack.orchestrate.engine.Orchestration;
 import org.dotwebstack.orchestrate.engine.schema.SchemaFactory;
 import org.dotwebstack.orchestrate.source.CollectionRequest;
@@ -52,19 +54,37 @@ public class GatewayConfiguration {
   }
 
   private Source createSourceStub() {
+    var num = new LinkedHashMap<String, Map<String, Object>>();
+    num.put("0200200000075716", Map.of("identificatie", "0200200000075716", "huisnummer", 701, "postcode", "7334DP",
+        "ligtAan", Map.of("identificatie", "0200300022472362")));
+    num.put("0200200000075717", Map.of("identificatie", "0200200000075717", "huisnummer", 702, "postcode", "7334DP",
+        "ligtAan", Map.of("identificatie", "0200300022472362")));
+    num.put("0200200000075718", Map.of("identificatie", "0200200000075718", "huisnummer", 703, "postcode", "7334DP",
+        "ligtAan", Map.of("identificatie", "0200300022472362"), "ligtIn", Map.of("identificatie", "2258")));
+
+    var vbo = new LinkedHashMap<String, Map<String, Object>>();
+    vbo.put("0200200000075716", Map.of("identificatie", "0200010000130331"));
+    vbo.put("0200200000075718", Map.of("identificatie", "0200010000130331"));
+
+    var opr = new LinkedHashMap<String, Map<String, Object>>();
+    opr.put("0200300022472362", Map.of("naam", "Laan van Westenenk", "ligtIn", Map.of("identificatie", "3560")));
+
+    var wpl = new LinkedHashMap<String, Map<String, Object>>();
+    wpl.put("3560", Map.of("naam", "Apeldoorn"));
+    wpl.put("2258", Map.of("naam", "Beekbergen"));
+
     return () -> new DataRepository() {
       @Override
       public Mono<Map<String, Object>> findOne(ObjectRequest objectRequest) {
         var typeName = objectRequest.getObjectType()
             .getName();
+        var objectKey = (String) objectRequest.getObjectKey()
+            .get("identificatie");
 
         return switch (typeName) {
-          case "Nummeraanduiding" ->
-              Mono.just(Map.of("identificatie", "0200200000075716", "huisnummer", 701, "postcode", "7334DP", "ligtAan",
-                  Map.of("identificatie", "0200300022472362")));
-          case "OpenbareRuimte" ->
-              Mono.just(Map.of("naam", "Laan van Westenenk", "ligtIn", Map.of("identificatie", "3560")));
-          case "Woonplaats" -> Mono.just(Map.of("naam", "Apeldoorn"));
+          case "Nummeraanduiding" -> Mono.just(num.get(objectKey));
+          case "OpenbareRuimte" -> Mono.just(opr.get(objectKey));
+          case "Woonplaats" -> Mono.just(wpl.get(objectKey));
           default -> Mono.error(() -> new RuntimeException("Error!"));
         };
       }
@@ -75,14 +95,11 @@ public class GatewayConfiguration {
             .getName();
 
         return switch (typeName) {
-          case "Nummeraanduiding" -> Flux.just(
-              Map.of("identificatie", "0200200000075716", "huisnummer", 701, "postcode", "7334DP", "ligtAan", Map.of(
-                  "identificatie", "0200300022472362")),
-              Map.of("identificatie", "0200200000075717", "huisnummer", 702, "postcode", "7334DP", "ligtAan", Map.of(
-                  "identificatie", "0200300022472362")),
-              Map.of("identificatie", "0200200000075718", "huisnummer", 703, "postcode", "7334DP", "ligtAan", Map.of(
-                  "identificatie", "0200300022472362")));
-          case "Verblijfsobject" -> Flux.just(Map.of("identificatie", "0200010000130331"));
+          case "Nummeraanduiding" -> Flux.fromIterable(num.values());
+          case "Verblijfsobject" -> Optional.ofNullable(vbo.get((String) collectionRequest.getFilter()
+                  .getValue()))
+              .map(Flux::just)
+              .orElse(Flux.empty());
           default -> Flux.error(() -> new RuntimeException("Error!"));
         };
       }
