d1e38095f1ac4ea63d1845fdcb663ba25d2e28f7
Pano Maria <pano.maria@gmail.com>
2023-02-07 09:54:24 +0100

    add sourcePath alias

--- parser-yaml/src/main/java/org/dotwebstack/orchestrate/parser/yaml/model/PropertyMappingMixin.java
+++ parser-yaml/src/main/java/org/dotwebstack/orchestrate/parser/yaml/model/PropertyMappingMixin.java
@@ -1,5 +1,6 @@
 package org.dotwebstack.orchestrate.parser.yaml.model;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import java.util.List;
 import lombok.extern.jackson.Jacksonized;
@@ -8,6 +9,7 @@ import org.dotwebstack.orchestrate.model.PropertyPath;
 @Jacksonized
 public abstract class PropertyMappingMixin {
 
+  @JsonAlias("sourcePath")
   @JsonFormat(with = JsonFormat.Feature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
   private List<PropertyPath> sourcePaths;
 }
