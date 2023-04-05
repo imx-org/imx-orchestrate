5fe539741ea8512577b4c70b811c886aa8a7bb05
Joost Farla <joost.farla@kadaster.nl>
2023-04-05 16:46:29 +0200

    Refactor batch loader

--- engine/src/main/java/org/dotwebstack/orchestrate/engine/fetch/NextOperation.java
+++ engine/src/main/java/org/dotwebstack/orchestrate/engine/fetch/NextOperation.java
@@ -1,12 +1,16 @@
 package org.dotwebstack.orchestrate.engine.fetch;
 
+import static java.util.function.Function.identity;
+import static org.dotwebstack.orchestrate.engine.fetch.FetchUtils.cast;
+import static org.dotwebstack.orchestrate.engine.fetch.FetchUtils.extractKey;
+
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 import lombok.Builder;
 import lombok.Getter;
+import org.dataloader.DataLoader;
+import org.dataloader.DataLoaderFactory;
 import org.dotwebstack.orchestrate.model.Property;
 import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
@@ -25,11 +29,49 @@ public class NextOperation {
   public Publisher<ObjectResult> apply(Flux<ObjectResult> resultFlux) {
     // TODO: handle only distinct inputs
     if (delegateOperation instanceof CollectionFetchOperation) {
-      return resultFlux.flatMap(this::fetchCollection);
+      return resultFlux.flatMapSequential(this::fetchCollection);
     }
 
-    return resultFlux.buffer()
-        .flatMap(this::execute);
+    var dataLoader = createDataLoader();
+
+    return resultFlux.doOnComplete(dataLoader::dispatch)
+        .flatMapSequential(objectResult -> {
+          var input = objectResult.getProperty(property.getName());
+
+          if (input == null) {
+            return Mono.just(objectResult);
+          }
+
+          if (input instanceof List<?>) {
+            return Mono.fromCompletionStage(dataLoader.loadMany(cast(input)))
+                .flatMapMany(Flux::fromIterable)
+                .collectList()
+                .map(nestedList -> objectResult.toBuilder()
+                    .nestedResult(property.getName(), CollectionResult.builder()
+                        .objectResults(nestedList)
+                        .build())
+                    .build());
+          }
+
+          return Mono.fromCompletionStage(dataLoader.load(cast(input)))
+              .map(nestedObject -> objectResult.toBuilder()
+                  .nestedResult(property.getName(), nestedObject)
+                  .build())
+              .defaultIfEmpty(objectResult);
+        });
+  }
+
+  private DataLoader<Map<String, Object>, ObjectResult> createDataLoader() {
+    return DataLoaderFactory.newMappedDataLoader(objectKeys -> {
+      var resultFlux = objectKeys.size() == 1 ?
+          delegateOperation.execute(FetchInput.newInput(objectKeys.iterator().next())) :
+          delegateOperation.executeBatch(objectKeys.stream()
+              .map(FetchInput::newInput)
+              .toList());
+
+      return resultFlux.collectMap(result -> extractKey(result.getType(), result.getProperties()), identity())
+          .toFuture();
+    });
   }
 
   private Publisher<ObjectResult> fetchCollection(ObjectResult objectResult) {
@@ -56,48 +98,5 @@ public class NextOperation {
             .nestedResult(property.getName(), nextResult)
             .build())
         .defaultIfEmpty(objectResult);
-  }
-
-  private Publisher<ObjectResult> execute(List<ObjectResult> objectResults) {
-    var distinctInputs = objectResults.stream()
-        .flatMap(objectResult -> Optional.ofNullable(inputMapper.apply(objectResult))
-            .stream())
-        .distinct()
-        .map(FetchInput::newInput)
-        .toList();
-
-    if (distinctInputs.isEmpty()) {
-      return Flux.fromIterable(objectResults);
-    }
-
-    if (distinctInputs.size() == 1) {
-      return Mono.just(distinctInputs.get(0))
-          .flatMapMany(nextInput -> delegateOperation.execute(nextInput)
-              .collect(Collectors.toMap(nextResult -> nextInput.getData(), Function.identity()))
-              .flatMapMany(nextResults -> combineResult(objectResults, nextResults)));
-    }
-
-    return delegateOperation.executeBatch(distinctInputs)
-        .collect(Collectors.toMap(ObjectResult::getKey, Function.identity()))
-        .flatMapMany(nextResults -> combineResult(objectResults, nextResults));
-  }
-
-  private Flux<ObjectResult> combineResult(List<ObjectResult> objectResults,
-      Map<Map<String, Object>, ObjectResult> nextResults) {
-    return Flux.fromIterable(objectResults)
-        .map(objectResult -> {
-          // TODO: Input mapping is now done twice
-          var nextInput = inputMapper.apply(objectResult);
-
-          if (nextInput == null) {
-            return objectResult;
-          }
-
-          return Optional.ofNullable(nextResults.get(nextInput))
-              .map(nextResult -> objectResult.toBuilder()
-                  .nestedResult(property.getName(), nextResult)
-                  .build())
-              .orElse(objectResult);
-        });
   }
 }
