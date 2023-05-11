b6aaa009cfa0d5a3f88e40605cf3286204e0ceb8
Joost Farla <joost.farla@kadaster.nl>
2023-05-11 14:12:08 +0200

    Remove unused annotation

--- parser-yaml/src/main/java/org/dotwebstack/orchestrate/parser/yaml/mixins/PropertyMappingMixin.java
+++ parser-yaml/src/main/java/org/dotwebstack/orchestrate/parser/yaml/mixins/PropertyMappingMixin.java
@@ -3,10 +3,8 @@ package org.dotwebstack.orchestrate.parser.yaml.mixins;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import java.util.List;
-import lombok.extern.jackson.Jacksonized;
 import org.dotwebstack.orchestrate.model.PathMapping;
 
-@Jacksonized
 public abstract class PropertyMappingMixin {
 
   @JsonAlias("pathMappings")
