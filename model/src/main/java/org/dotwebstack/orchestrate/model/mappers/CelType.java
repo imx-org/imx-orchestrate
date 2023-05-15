27826531c374b0f5460da5d0d305ec75b61655b2
Joost Farla <joost.farla@kadaster.nl>
2023-05-15 11:04:05 +0200

    Basic CEL mapper implementation

--- model/src/main/java/org/dotwebstack/orchestrate/model/mappers/CelType.java
+++ model/src/main/java/org/dotwebstack/orchestrate/model/mappers/CelType.java
@@ -1,7 +1,13 @@
 package org.dotwebstack.orchestrate.model.mappers;
 
+import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
+import org.dotwebstack.orchestrate.model.ModelException;
+import org.projectnessie.cel.checker.Decls;
+import org.projectnessie.cel.tools.Script;
+import org.projectnessie.cel.tools.ScriptCreateException;
+import org.projectnessie.cel.tools.ScriptException;
+import org.projectnessie.cel.tools.ScriptHost;
 
 public class CelType implements ResultMapperType {
 
@@ -12,7 +18,29 @@ public class CelType implements ResultMapperType {
 
   @Override
   public ResultMapper create(Map<String, Object> options) {
-    // TODO: Implement CEL evaluation
-    return Objects::nonNull;
+    var scriptHost = ScriptHost.newBuilder()
+        .build();
+
+    var expr = (String) options.get("expr");
+    Script script;
+
+    try {
+      script = scriptHost.buildScript(expr)
+          .withDeclarations(Decls.newVar("result", Decls.Any))
+          .build();
+    } catch (ScriptCreateException e) {
+      throw new ModelException("Could not parse expression: " + expr, e);
+    }
+
+    return result -> {
+      var arguments = new HashMap<String, Object>();
+      arguments.put("result", result);
+
+      try {
+        return script.execute(Object.class, arguments);
+      } catch (ScriptException e) {
+        throw new ModelException("Could not evaluate expression: " + expr, e);
+      }
+    };
   }
 }
