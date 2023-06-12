0cef298c02952781572cb20d69dbebbe4dfdb28c
Joost Farla <joost.farla@kadaster.nl>
2023-06-12 14:54:55 +0200

    Fix IT

--- parser-yaml/src/test/java/org/dotwebstack/orchestrate/parser/yaml/YamlModelMappingParserTest.java
+++ parser-yaml/src/test/java/org/dotwebstack/orchestrate/parser/yaml/YamlModelMappingParserTest.java
@@ -2,13 +2,14 @@ package org.dotwebstack.orchestrate.parser.yaml;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.dotwebstack.orchestrate.model.ComponentFactory;
 import org.junit.jupiter.api.Test;
 
 class YamlModelMappingParserTest {
 
   @Test
   void mapWorks() {
-    var yamlMapper = YamlModelMappingParser.getInstance();
+    var yamlMapper = new YamlModelMappingParser(new ComponentFactory());
     var inputStream = YamlModelMappingParser.class.getResourceAsStream("/adresmapping.yaml");
 
     var mapping = yamlMapper.parse(inputStream);
