048b4348d66db2f0d3ff0625de8f7a2ba483b52e
Joost Farla <joostfarla@users.noreply.github.com>
2023-06-29 12:37:10 +0200

    Refs construct for GraphQL source (#91)
    * Added ref construction
    
    * Remove debug logging
    
    * Remove debug logging
    
    * Fix NPE
    
    ---------
    
    Co-authored-by: Michel ten Voorde <michel.tenvoorde@kadaster.nl>

--- source-graphql/src/main/java/org/dotwebstack/orchestrate/source/graphql/mapper/ResponseMapper.java
+++ source-graphql/src/main/java/org/dotwebstack/orchestrate/source/graphql/mapper/ResponseMapper.java
@@ -4,9 +4,10 @@ import static org.dotwebstack.orchestrate.source.graphql.mapper.MapperConstants.
 import static org.springframework.util.StringUtils.uncapitalize;
 
 import graphql.ExecutionResult;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.dotwebstack.orchestrate.source.graphql.config.GraphQlOrchestrateConfig;
 import reactor.core.publisher.Flux;
@@ -18,17 +19,21 @@ public class ResponseMapper {
   private final GraphQlOrchestrateConfig config;
 
   public Mono<Map<String, Object>> processFindOneResult(Mono<ExecutionResult> executionResult) {
-    return executionResult.map(result -> Objects.requireNonNullElse(result.getData(), Map.of()));
+    return executionResult.map(result -> Optional.ofNullable((Map<String, Object>) result.getData())
+            .orElse(Map.of()))
+        .map(ResponseMapper::unwrapRefs);
   }
 
   public Flux<Map<String, Object>> processFindResult(Mono<ExecutionResult> executionResult, String objectName) {
     return executionResult.map(e -> getCollectionResult(e, objectName))
-        .flatMapMany(Flux::fromIterable);
+        .flatMapMany(Flux::fromIterable)
+        .map(ResponseMapper::unwrapRefs);
   }
 
   public Flux<Map<String, Object>> processBatchResult(Mono<ExecutionResult> executionResult, String objectName) {
     return executionResult.map(e -> getBatchResult(e, objectName))
-        .flatMapMany(Flux::fromIterable);
+        .flatMapMany(Flux::fromIterable)
+        .map(ResponseMapper::unwrapRefs);
   }
 
   private List<Map<String, Object>> getCollectionResult(ExecutionResult executionResult, String objectName) {
@@ -41,5 +46,24 @@ public class ResponseMapper {
   private List<Map<String, Object>> getBatchResult(ExecutionResult executionResult, String objectName) {
     var data = executionResult.getData();
     return ((Map<String, List<Map<String, Object>>>) data).get(uncapitalize(objectName) + config.getBatchSuffix());
+  }
+  private static Map<String, Object> unwrapRefs(Map<String, Object> item) {
+    return item.entrySet()
+        .stream()
+        .collect(HashMap::new, (acc, e) -> {
+          var value = e.getValue();
+
+          if (value instanceof Map<?, ?> mapValue) {
+            if (mapValue.containsKey("ref")) {
+              value = mapValue.get("ref");
+            }
+
+            if (mapValue.containsKey("refs")) {
+              value = mapValue.get("refs");
+            }
+          }
+
+          acc.put(e.getKey(), value);
+        }, HashMap::putAll);
   }
 }
