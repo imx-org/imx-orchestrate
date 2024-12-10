25d1dd69a855cb0bd839110a516c29ec15d1cb93
Joost Farla <joost@qonduct.io>
2024-12-10 13:14:54 +0100

    Fix boolean result

--- model/src/main/java/nl/geostandaarden/imx/orchestrate/model/mappers/IsEmptyMapperType.java
+++ model/src/main/java/nl/geostandaarden/imx/orchestrate/model/mappers/IsEmptyMapperType.java
@@ -3,6 +3,8 @@ package nl.geostandaarden.imx.orchestrate.model.mappers;
 import java.util.Collection;
 import java.util.Map;
 import nl.geostandaarden.imx.orchestrate.model.ModelException;
+import nl.geostandaarden.imx.orchestrate.model.lineage.PathExecution;
+import nl.geostandaarden.imx.orchestrate.model.result.PathResult;
 
 public final class IsEmptyMapperType implements ResultMapperType {
 
@@ -15,7 +17,11 @@ public final class IsEmptyMapperType implements ResultMapperType {
     public ResultMapper create(Map<String, Object> options) {
         return (result, property) -> {
             if (result.isNull()) {
-                return result;
+                // TODO: Where to get path execution from?
+                return PathResult.builder() //
+                        .pathExecution(PathExecution.builder().build())
+                        .value(true)
+                        .build();
             }
 
             if (result instanceof Collection<?> collection) {
--- model/src/main/java/nl/geostandaarden/imx/orchestrate/model/mappers/IsNotEmptyMapperType.java
+++ model/src/main/java/nl/geostandaarden/imx/orchestrate/model/mappers/IsNotEmptyMapperType.java
@@ -3,6 +3,8 @@ package nl.geostandaarden.imx.orchestrate.model.mappers;
 import java.util.Collection;
 import java.util.Map;
 import nl.geostandaarden.imx.orchestrate.model.ModelException;
+import nl.geostandaarden.imx.orchestrate.model.lineage.PathExecution;
+import nl.geostandaarden.imx.orchestrate.model.result.PathResult;
 
 public final class IsNotEmptyMapperType implements ResultMapperType {
 
@@ -15,7 +17,11 @@ public final class IsNotEmptyMapperType implements ResultMapperType {
     public ResultMapper create(Map<String, Object> options) {
         return (result, property) -> {
             if (result.isNull()) {
-                return result;
+                // TODO: Where to get path execution from?
+                return PathResult.builder() //
+                        .pathExecution(PathExecution.builder().build())
+                        .value(false)
+                        .build();
             }
 
             if (result instanceof Collection<?> collection) {
